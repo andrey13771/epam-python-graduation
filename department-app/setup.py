60e831664a1390fffed4f872c88d1b9011760db9
Andrii Pavlenko <andrey13771@gmail.com>
2019-12-25 16:20:31 +0200

    created project structure

---
+++ department-app/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='department-app',
+    version='1.0',
+    description='A simple web app for managing departments and employees',
+    author='Andrii Pavlenko',
+    author_email='andrey13771@gmail.com',
+)
